86952120c826a191cf8def81979c922e83902ac3
99cm <zyizyao@china-guide.com>
2018-05-17 20:43:06 -0400

    Update install_generator.rb

--- lib/generators/spree_contact_us/install/install_generator.rb
+++ lib/generators/spree_contact_us/install/install_generator.rb
@@ -2,11 +2,19 @@ module SpreeContactUs
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      argument :file_name, :type => :string, :desc => 'rails app_path', :default => '.'
+      argument :file_name, type: :string, desc: 'rails app_path', default: '.'
       source_root File.expand_path('../../templates', __FILE__)
 
       def copy_initializer_file
         template 'spree_contact_us.rb', "#{file_name}/config/initializers/spree_contact_us.rb"
+      end
+
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_contact_us\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', "*= require spree/frontend/spree_contact_us\n", before: /\*\//, verbose: true
       end
 
     end
